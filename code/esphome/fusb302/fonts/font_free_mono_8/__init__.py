c4edddb5613b46880ee68e6b7937e33e732021f9
rbaron <rbaron@rbaron.net>
2024-05-08 08:37:17 +0200

    Big cleanup

--- code/esphome/fusb302/fonts/font_free_mono_8/__init__.py
+++
@@ -1,6 +0,0 @@
-# SPDX-FileCopyrightText: 2023 Jeff Epler for Adafruit Industries
-# SPDX-License-Identifier: Unlicense
-
-# CircuitPython font generated from fonts/FreeMono.ttf @8
-from adafruit_bitmap_font import bitmap_font
-FONT = bitmap_font.load_font(__file__.rsplit("/", 1)[0] + "/font.pcf")
